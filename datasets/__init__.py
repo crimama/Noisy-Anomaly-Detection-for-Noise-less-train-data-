92389056b70c39a7cfe16bde23bb19f0e55a4931
crimama <crimama2@gmail.com>
2023-08-31 07:57:48 +0000

    commit

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
+from .cifar10 import CIFAR10
+from .mvtecad import MVTecAD
 from .factory import create_dataset
 from .augmentation import train_augmentation, test_augmentation, gt_augmentation
-from .build import AnomalyDataset, MVTecAD
