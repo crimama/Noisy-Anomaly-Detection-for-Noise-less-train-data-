2b504544f370d8789baf417307fe84f110cad6a8
crimama <crimama2@gmail.com>
2023-10-22 23:04:54 +0900

    [feat] add fastflow, rd

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from .simclr import ResNetSimCLR
 from .stpm import STPM 
 from .patchcore import PatchCore
 from .anomalib_stpm import AnomalibSTPM
+from .fastflow import FastFlow 
+from .reversedistillation import ReverseDistillation
